37baf41198514e1a50bea630aae2ed7cd7d4fed1
pierrepremel <pierrepremel@gmail.com>
2025-10-31 10:37:29 +0100

    Add Footer component

---
+++ app/components/Footer.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items: NavigationMenuItem[] = [
+  {
+    label: 'Conditions générales des ventes',
+    to: '/#',
+    active: false,
+  },
+
+  {
+    label: 'Conditions générales des expéditions',
+    to: '/#',
+    active: false,
+  },
+  {
+    label: 'Politique de confidentialité',
+    to: '/#',
+    active: false,
+  },
+  {
+    label: 'Mentions légales',
+    to: '/#',
+    active: false,
+  },
+  {
+    label: 'Support',
+    to: '/#',
+    active: false,
+  },
+]
+</script>
+
+<template>
+  <div>
+    <USeparator type="dashed" class="h-px" />
+
+    <UFooter>
+      <template #left>
+        <p class="text-muted text-sm">
+          ADN Enchères - © {{ new Date().getFullYear() }}
+        </p>
+      </template>
+
+      <template #right>
+        <UNavigationMenu :items="items" variant="link" />
+      </template>
+    </UFooter>
+  </div>
+</template>
+
+<style></style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,6 +6,7 @@
         <slot />
       </UPage>
     </UContainer>
+    <Footer />
   </div>
 </template>
 
